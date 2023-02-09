230f0016a76eeab7d524d6eb1f0dfcbe81420666
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-09 15:00:23 -0400

    Server side - added typescript

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -4,8 +4,9 @@ import { DB_URI } from "./constants";
 export default class Database {
   static async ConnectDB() {
     try {
-      await mongoose.connect(DB_URI);
-      console.log("Connected to the database successfully");
+      await mongoose.connect(DB_URI).then(() => {
+        console.log("Connected to the database successfully");
+      });
     } catch (error) {
       console.log(error);
     }
